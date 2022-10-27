e4a44af3bd127acc93a8013fcd9231167fb57ec7
harutmovsisyan <harutmovsisyan@yahoo.com>
2022-10-27 23:11:50 +0400

    adminlte fresh add 27.10.22

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
+Route::get('admin', function () {
+    return view('admin/index');
+});
 
 //Route::group('', function (){
 //    Route::get('/', 'indexController');
